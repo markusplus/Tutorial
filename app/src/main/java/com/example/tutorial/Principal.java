649d92d507e3273ba8249367766c48652e9a1f94
markusplus <marcjovani16@gmail.com>
2022-10-06 12:48:21 +0200

    Actualizado

--- app/src/main/java/com/example/tutorial/Principal.java
+++ app/src/main/java/com/example/tutorial/Principal.java
@@ -2,6 +2,7 @@ package com.example.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,17 +10,26 @@ import android.widget.Button;
 public class Principal extends AppCompatActivity {
 
     Button empezar ;
+    Button autor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
         empezar = findViewById(R.id.inicio_button);
+        autor = findViewById(R.id.autor_button);
 
         empezar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.activity_main);
+                startActivity(new Intent(Principal.this, MainActivity.class));
+            }
+        });
+
+        autor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Principal.this, Autor.class));
             }
         });
     }
---
+++ app/src/main/java/com/example/tutorial/ZonaIntercambio.java
@@ -0,0 +1,6 @@
+package com.example.tutorial;
+
+public class ZonaIntercambio {
+    public static String nombre ="Nadie";
+    public static int dinero = 0;
+}
